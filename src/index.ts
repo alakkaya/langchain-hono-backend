ecd1baad787765feb26fa100dd9a9af1d32277f3
aliAkkayaJ <aliakkayaa0@gmail.com>
2024-08-04 19:59:44 +0300

    loadTextEmbeddings created 1.0

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import path from 'path'
 import { promises as fs } from 'fs'
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
 const app = new Hono()
 
@@ -15,6 +16,16 @@ const getTextFile = async () => {
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+
+app.get('/loadTextEmbeddings',async (c) => {
+const text = await getTextFile();
+
+const splitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 1000,
+  separators:['\n\n', '\n', ' ', '', '###'],
+  chunkOverlap:50
+});
 })
 
 const port = 3002
